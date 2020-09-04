3eae76b09d851552dd659b1d902abc9082a78df0
Alfonova133 <70742362+Alfonova133@users.noreply.github.com>
2020-09-04 14:23:03 +0800

    upload file web portfolio
    upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const txtElement = ['Freelancer','Web Developer','Graphic Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+ if(count ==txtElement.length){
+     count = 0;
+ }
+ currentTxt = txtElement[count];
+ words = currentTxt.slice(0, ++txtIndex);
+ document.querySelector('.efek-ngetik').textContent = words;
+setTimeout(ngetik, 500);
+})(;)
